4300960f0639cf23953b90ee3abb124e48638f92
Ben Anderman <ben@happyspork.com>
2026-01-19 00:45:23 -0500

    Make gen rules script support instructions with constants

--- gen_customasm_rules.pl
+++ gen_customasm_rules.pl
@@ -11,10 +11,10 @@ my %name_map = (
     "MADR"         => "MAdr",
     "STCK"         => "Stack",
     "OUTA"         => "OutA",
-    "OUTB"         => "OutB",
+    "SHFT"         => "Shift",
     "SWAP"         => "Swap",
-    "INPA"         => "InA",
-    "INPB"         => "InB",
+    "IOA"          => "InOutA",
+    "IOB"          => "InOutB",
     "PCNT_BSELECT" => "HighB",
     "MADR_BSELECT" => "HighB",
 );
@@ -43,18 +43,23 @@ if ($instructions_file =~ /Instruction instructions\[\] = \{(.+?)\}/s) {
     my $instructions_block = $1;
     my $index = 0;
     my @rules;
-    while ($instructions_block =~/Type::(\w+?),.*?FlagsMask::(\w+)(?:\s*\|.*FlagsMask::(\w+))?,\s*(\d)(?:, (\w+))?(?:, (\w+))?(?:, (\w+))?/g) {
+    while ($instructions_block =~/    Instruction\(Instruction::Type::(\w+?),.*?FlagsMask::(\w+)(?:\s*\|.*FlagsMask::(\w+))?,\s*(CONST_(?:ZERO|ONE)\()?(\d)\)?(?:, (\w+))?(?:, (\w+))?(?:, (\w+))?/g) {
         my $arg_template = "";
         my $arg_value = "";
         my $flags1 = $2;
         my $flags2 = $3 ? "|$3" : "";
         my $flags_suffix = $flags_mapping{"$flags1$flags2"};
         my $inst = "$1$flags_suffix";
-        my $data_bytes = $4;
-        my $var1 = mappedName($5);
-        my $var2 = defined $6 ? ", ".mappedName($6) : "";
-        my $var3 = defined $7 ? ", ".mappedName($7) : "";
+        my $data_bytes = $5;
+        my $var1 = mappedName($6);
+        my $var2 = defined $7 ? ", ".mappedName($7) : "";
+        my $var3 = defined $8 ? ", ".mappedName($8) : "";
         my $vars = "$var1$var2$var3";
+        my $zero_suffix = "  ";
+        if (defined $4) {
+            $data_bytes--;
+            $zero_suffix = ($4 eq "CONST_ONE(") ? "01" : "00";
+        }
         if ($data_bytes != 0) {
             $arg_template = $data_bytes == 1 ? "{value}" : "{address}";
             $arg_value = $data_bytes == 1 ? "\@ value`8" : "\@ address`16";
@@ -63,7 +68,7 @@ if ($instructions_file =~ /Instruction instructions\[\] = \{(.+?)\}/s) {
                 $arg_value = "\@ address`16 \@ value`8";
             }
         }
-        my $rule = sprintf("%-14s %-20s %-20s => 0x%02X %s", $inst, $vars, $arg_template, $index, $arg_value);
+        my $rule = sprintf("%-14s %-20s %-20s => 0x%02X%s %-27s %s", $inst, $vars, $arg_template, $index, $zero_suffix, $arg_value, ($index & 1 << 4) ? "; Has 5th bit" : "");
         push(@rules, ($rule));
         $index++;
     }
