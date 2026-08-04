1bc68c4bb620fc951f81b8769c42ad96833d1911
Ben Anderman <ben@happyspork.com>
2026-08-04 12:36:32 -0400

    Add 2 player snake to C++

--- Code/programs.h
+++ Code/programs.h
@@ -6,6 +6,7 @@
 uint8_t getMovingDotByte(uint16_t address);
 uint8_t getJumpToCartridgeByte(uint16_t address);
 uint8_t getSnakeByte(uint16_t address);
+uint8_t getSnake2PByte(uint16_t address);
 uint8_t getMemEditByte(uint16_t address);
 
 #endif
