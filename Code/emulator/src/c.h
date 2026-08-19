a5012ab54b8c4a47144ff5a0d30351405a899c59
Andrew Kelley <andrew@ziglang.org>
2026-08-18 22:47:31 -0700

    update zig code to 0.17.0-dev.1821+fff5e675b

---
+++ Code/emulator/src/c.h
@@ -0,0 +1,7 @@
+#define SDL_DISABLE_OLD_NAMES
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_revision.h>
+// We are providing our own entry point
+#define SDL_MAIN_HANDLED
+#include <SDL3/SDL_main.h>
+#include <spork_8_state.h>
